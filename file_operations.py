6b6f76addbe532c26baba86410c924830da4d370
محمد الصوالحي <ms.2052001@gmail.com>
2023-03-01 20:12:25 +0200

    refactor and add check_board function

--- file_operations.py
+++ file_operations.py
@@ -1,63 +1,77 @@
 import sudoku
 import time
-def write_board (board,flag,filename) :
-    """ 
-    Write Board With Generate Puzzle 
-    parameter => board and choise write puzzle or solution and filname (solution or puzzle name)
-    return => void
+
+
+def write_board(board, filename):
+    """Write board to a file
+
+    Parameters
+    ----------
+        board : list[list[int]]
+
+        filename : str
     """
-    # flag knows me to choose the board 
-    if flag :
-          board = sudoku.remove_field(board)
-     # We will open the file in which we will write
-    with open(__file__.replace("file_operations.py",f"{filename}.txt"), "w") as f:
+
+    filename = __file__.replace("file_operations.py", f"{filename}.txt")
+
+    dashes = ("-" * 9).join([" "] * 4) * 3 + "\n"
+    bars = (" " * 9).join(["|"] * 4) * 3 + "\n"
+
+    with open(filename, "w") as f:
         for i in range(9):
-            f.write(" --------- --------- ---------  --------- --------- ---------  --------- --------- --------- \n")
-            f.write("|         |         |         ||         |         |         ||         |         |         |\n"*2)
+            f.write(dashes)
+            f.write(bars)
+            f.write(bars)
             for j in range(9):
                 f.write("|")
-                if j == 3 or j == 6 :
-                     f.write("|")
-                if board[i][j] != 0 :
-                     f.write(f"    {board[i][j]}    ")
-                else :
-                     f.write("         ")
-                if j == 8 :
-                     f.write("|")
-            f.write("\n|         |         |         ||         |         |         ||         |         |         |"*2)
-            if i == 2 or i == 5 :
-                 f.write("\n --------- --------- ---------  --------- --------- ---------  --------- --------- --------- ")
-            f.write("\n")
-        f.write(" --------- --------- ---------  --------- --------- ---------  --------- --------- --------- \n\n\n")
-    print(__file__.replace("file_operations.py",f"{filename}.txt"))
+                if j == 3 or j == 6:
+                    f.write("|")
+                if board[i][j] != 0:
+                    f.write(f"    {board[i][j]}    ")
+                else:
+                    f.write("         ")
+            f.write("|\n")
+            f.write(bars)
+            f.write(bars)
+            f.write(dashes)
+            if i == 2 or i == 5:
+                f.write(dashes)
+        f.write(dashes + "\n\n")
 
-# generate complete board
-board = sudoku.generate_board()
-# copy orignal board before creating puzzle 
-solution_board = [[j for j in i] for i in board]
-# generate puzzle board and write in file
-write_board(board,True,'sudoku_game')
-print("You Can Play Sudoku Game Now")
-start_time = time.time()
-choose = input("If You Finish Game Enter (y or yes) : ").strip().lower()
-if choose == 'y' or choose == 'yes' :
-     end_time = time.time()
+    print(filename)
 
-# Ask Users To Save Original Solution
-while True :
-     try :
-          ch = input("You Need To Show Solution(y or n ) : ").strip().lower()
-          if ch == 'y' or ch == 'yes' :
-               write_board(solution_board,False,' solution')
-               break
-          elif ch == 'n'  or ch == 'no' :
-               break
-          else :
-               raise ValueError
-     except ValueError :
-          print("Plese Enter Valid Choise!")
-     except KeyboardInterrupt:
-          print("\nGood Bye!")
-          exit()
-time_taken = end_time - start_time
-print(f"Time taken: {time_taken // 60:.0f} minutes {time_taken%60:.0f} Seconds ")
+
+def main():
+    # generate complete board
+    solution = sudoku.generate_board()
+
+    # copy orignal board and make empty cells
+    board = [i.copy() for i in solution]
+    board = sudoku.remove_fields(board)
+
+    # generate puzzle board and write in file
+    print("Sudoku game is written in:")
+    write_board(board, 'sudoku_game')
+    print("\nYou can play sudoku game now")
+
+    start_time = time.time()
+    choose = input("If you finish game enter (y or yes): ").strip().lower()
+    if choose == 'y' or choose == 'yes':
+        end_time = time.time()
+        time_taken = end_time - start_time
+        print(
+            f"Time taken: {time_taken // 60:.0f} minutes {time_taken%60:.0f} Seconds ")
+
+    # ask users to save original solution
+    ans = input("Do you need to show solution(y or yes): ").strip().lower()
+    if ans == 'y' or ans == 'yes':
+        print("Solution file is written to:")
+        write_board(solution, 'solution')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt, EOFError):
+        print("\nGood Bye! 👋")
+        exit()
